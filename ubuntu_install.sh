554d933b3e1f768796e4e4d314635faa1cac9652
victor <victormoreira176@hotmail.com>
2021-02-18 08:47:09 -0300

    :sparkles: adding slack

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-apps = "i3 rxvt ranger neofetch polybar wal postman compton flameshot code vim npm nodejs"
+apps = "i3 rxvt ranger neofetch polybar wal postman compton flameshot code vim npm nodejs slack"
 
 echo updating packages
 apt-get update
