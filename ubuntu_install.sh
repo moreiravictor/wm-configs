2bdeada709bdf4164e546ba540f5d9389cedb3c8
Victor Moreira <victormoreira176@hotmail.com>
2021-08-29 01:01:38 -0300

    :sparkles: update install script

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -1,8 +1,26 @@
 #!/bin/sh
-apps = "i3 rxvt ranger neofetch polybar wal postman compton flameshot code vim npm nodejs slack"
 
-echo updating packages
+#colors
+RED='\033[0;31m'
+PURPLE='\033[0;32m'
+CYAN='\033[1;36m'
+NC='\033[0m'
+YELLOW='\033[1;33m'
+
+#vars
+user=$(whoami)
+APT_APPS="i3 rxvt ranger neofetch compton flameshot vim npm nodejs slack"
+SNAP_APPS="code postman wal polybar discord"
+
+#commands
+echo "${PURPLE}hey there! everything is gonna be setup 4u, $user <3${NC}"
+
+echo "${CYAN}updating packages${NC}"
+
 apt-get update
 
-echo installing default apps
-apt-get install $apps -y
+echo "${YELLOW}installing apt apps${NC}"
+apt-get install $APT_APPS -y
+
+echo "${YELLOW}installing snap apps${NC}"
+snap install $SNAP_APPS -y
